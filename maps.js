22a4d8faf00a090810dd433ab5e4b4be5caf1250
Igor Nascimento Saturnino <60020476+IgorSaturno@users.noreply.github.com>
2020-05-10 23:59:09 -0300

    Add files via upload

--- maps.js
+++ maps.js
@@ -1,15 +1,50 @@
-function inicializar() {
-	var coordenadas = {lat: -22.912869, lng: -43.228963};
+var map;
+var centerPos = new google.maps.LatLng(-23.558126,-46.645255);
+var zoomLevel = 13;
+function initialize() {
+	var mapOptions = {
+		center: centerPos,
+		zoom: zoomLevel
+	};
+	map = new google.maps.Map( document.getElementById("map-canvas"), mapOptions );
 
-	var mapa = new google.maps.Map(document.getElementById('mapa'), {
-		zoom: 15,
-		center: coordenadas
-	});
+	var locations = [
+	['Apartment in Son Parc, wonderful views', 40.0190632, 4.1615008],
+	['APARTAMENTO IDEAL PAREJAS EN SON PARC', 40.018638, 4.162226],
+	['Casa Charmosa Bem Localizada', -22.970288, -42.025667],
+	['Quartos pertinho do Anhembi!', -23.5134004, -46.6563539],
+	['Charming Chalet, private pool, free AC & WiFi', 40.025297, 4.164493],
+	['Menorca Green Park C7', 40.020445, 4.162369],
+	['Villa Monty', 40.019230, 4.168149],
+	['Bonito penthouse cerca del aeropuerto CDMX', 19.4360762, -99.0719083],
+	['The Backpackers Retreat', -17.5210415, 146.0302536],
+	['10min airport, Foro Sol, Palacio de los Deportes', 19.4055251, -99.0985425],
+	['Acogedor depto cerca aeropuerto, foro Sol, Centro', 40.019230, 4.168149],
+	['Sol Nascente da Prainha',	-22.9615953, -42.0249106],
+	['Ap novo com wi-fi na Prainha - Arraial do Cabo', -22.9620016,	-42.025439],
+	['Suite @ Subway Trianon Masp',-23.565591, -46.654429],
+	['Quarto Arouche Centro SP Ótima Localização', -23.5386407,-46.6458618],
+	['Quarto 1 - Parque da Aclimacão', -23.5721091,-46.6289573],
+	['Existe amor em SP', -23.571721, -46.631318],
+	['Confortável quarto de casal px. à Av. Paulista', -23.569880, -46.646282],
+	['Room Higienopolis São Paulo', -23.546238, -46.660215],
+	['Confortável quarto solteiro px. à Av. Paulista', -23.569308, -46.646437],
+	['Modern, Cozy + all you need! Muito aconchegante!!', -23.558126, -46.645255],
+	['Recanto Rosana - Quartos e estrutura completa', -23.529724, -46.663611],
+	['Suite na Montanha - Refúgio do Artista', -23.479654, -46.690013],
+	['Sítio Costa do Sol', -23.498228, -46.612784],
+];
+var image = 'imagens/map-marker-alt-solid.svg';
 
-	var marker = new google.maps.Marker({
-		position: coordenadas,
-		map: mapa,
-		title: 'Meu marcador'
-	});
+
+for (i = 0; i < locations.length; i++) {
+marker = new google.maps.Marker({
+position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+	title: locations[i][0],
+	map: map,
+	icon: image
+});
 }
-		AIzaSyCRQA3JvqDuQNZ9V8GjhTN8hwqOM31Eed0
+
+}
+google.maps.event.addDomListener(window, 'load', initialize);
--- script.js
+++ script.js
@@ -1,49 +1,60 @@
+var apartments = [];
+
+function differenceInDays(firstDate, secondDate) {
+	return Math.round((secondDate.getTime() - firstDate.getTime()) / (1000*60*60*24));
+}
+
+function listApartments() {
+	let list = document.getElementById('list');
+	list.innerHTML = '';
+
+	var checkIn = new Date(document.getElementById("inputCheckIn").value);
+	var checkOut = new Date(document.getElementById("inputCheckOut").value);
+
+	var differenceD = differenceInDays(checkIn, checkOut);
+
+	for(let i = 0; i < apartments.length; i++) {
+		let card = document.createElement('div');
+		card.className = 'list-group-item listcolun';
+
+		let img = document.createElement('div');
+		img.style.backgroundImage = 'url(' + apartments[i].photo + ')';
+		img.className = 'list-img';
+		card.appendChild(img);
+
+		let cardText = document.createElement('div');
+		cardText.className = 'card-text';
+		card.appendChild(cardText);
+
+		let title = document.createElement('small');
+		title.appendChild(document.createTextNode(apartments[i].property_type));
+		cardText.appendChild(title);
+
+		let name = document.createElement('h4');
+		name.appendChild(document.createTextNode(apartments[i].name));
+		cardText.appendChild(name);
+
+		let price = document.createElement('div');
+		price.innerHTML = '<b>R$'+ (apartments[i].price) + '</b>/noite';
+		cardText.appendChild(price);
+
+		if (!isNaN(differenceD)) {
+			let total = document.createElement('div');
+			total.innerHTML = 'Total de R$'+ (apartments[i].price * differenceD);
+			cardText.appendChild(total);
+		}
+		list.appendChild(card);
+	}
+}
+
 var url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";//Sua URL
 
 var xhttp = new XMLHttpRequest();
 xhttp.open("GET", url, true);
-xhttp.onreadystatechange = function(){//Função a ser chamada quando a requisição retornar do servidor
+xhttp.onreadystatechange = function() {//Função a ser chamada quando a requisição retornar do servidor
 		if ( this.readyState == 4 && this.status == 200 ) {//Verifica se o retorno do servidor deu certo
-			var apartments = JSON.parse(this.responseText);
-
-			for(let i = 0; i < apartments.length; i++) {
-				let card = document.createElement('div');
-				card.className = 'list-group-item listcolun';
-
-				let img = document.createElement('div');
-				img.style.backgroundImage = 'url(' + apartments[i].photo + ')';
-				img.className = 'list-img';
-				card.appendChild(img);
-
-				let cardText = document.createElement('div');
-				cardText.className = 'card-text';
-				card.appendChild(cardText);
-
-				let title = document.createElement('small');
-				title.className = 'card-text';
-				title.appendChild(document.createTextNode(apartments[i].property_type));
-				cardText.appendChild(title);
-
-				let name = document.createElement('h4');
-				name.className = 'card-text';
-				name.appendChild(document.createTextNode(apartments[i].name));
-				cardText.appendChild(name);
-
-				let price = document.createElement('h6');
-				price.className = 'card-text';
-				price.appendChild(document.createTextNode('R$ '+apartments[i].price));
-				cardText.appendChild(price);
-
-				let list = document.getElementById('list');
-				list.appendChild(card);
-
-				console.log(card, apartments[i]);
-				console.log(apartments[i].name);
-			}
-
-
-
-
+			apartments = JSON.parse(this.responseText);
+			listApartments();
 	}
 }
 
